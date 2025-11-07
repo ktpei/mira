eaa2c11bcb28f6ab8b182a97b2d427b90c8d92ff
ktpei <kevinxpei@mgmail.com>
2025-11-06 20:56:11 -0500

    Barebone tab structure

--- app/(tabs)/gallery.tsx
+++ app/(tabs)/gallery.tsx
@@ -3,12 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-export default function FeedScreen() {
+export default function GalleryScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Feed</Text>
+      <Text style={styles.title}>Gallery</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <EditScreenInfo path="app/(tabs)/gallery.tsx" />
     </View>
   );
 }
@@ -29,3 +29,4 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
